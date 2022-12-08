8c3ec2d265b3138b0e6f274374b75bbcdb1cfb40
shashank <shashank944533@gmail.com>
2022-12-08 19:09:35 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a,b;
+    char operation ;
+
+    cout<<"Enter first number";
+    cin>>a;
+
+    cout<<"Enter Operation(+,-,*,/)";
+    cin>>operation;
+
+    cout<<"Enter second number";
+    cin>>b;
+
+    switch(operation)
+    {
+        case '+':
+        cout<<"Result = "<<a+b;
+        break;
+
+        case '-':
+        cout<<"Result = "<<a-b;
+        break;
+
+        case '*':
+        cout<<"Result = "<<a*b;
+        break;
+
+        case '/':
+        cout<<"Result =" <<a/b;
+        break;
+
+        default :
+            cout<<"Invalid operation"<<endl;
+    }
+
+}
